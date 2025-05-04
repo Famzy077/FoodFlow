6b641c7ae71c66644d23f1ede051d335143b3745
Akinola Femi <akinolafemi573@gmail.com>
2025-05-04 15:52:47 +0100

    Axios error fixed

--- Client/FoodFlow/src/services/AuthService.js
+++ Client/FoodFlow/src/services/AuthService.js
@@ -1,45 +1,6 @@
-// import { delay } from "@/lib/Delay";
-// import apiClient from "./AxiosApiClient";
-
-// const login = "/auth/login";
-// const register = "/auth/sign-up";
-// const AuthService = {};
-
-// AuthService.login = async (loginData) => {
-//   const headers = {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   };
-
-//   try {
-//     const res = await apiClient.post(login, loginData, headers);
-//     return res.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// AuthService.register = async (sellerData) => {
-//   const headers = {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   };
-
-//   try {
-//     const res = await apiClient.post(register, sellerData, headers);
-//     return res.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// export default AuthService;
-
 import apiClient from "./AxiosApiClient";
 
-const loginEndpoint = "/auth/login";
+const loginEndpoint = "/authauth/login";
 const registerEndpoint = "/auth/sign-up";
 
 const AuthService = {};
